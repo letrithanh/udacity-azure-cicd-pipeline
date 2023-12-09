dc6cd35b809042d368f5f062cdd3cb9f275aa2cd
Thanh Le <letrithanh@outlook.com>
2023-12-09 14:43:58 +0700

    remove logs

--- commands.sh
+++ commands.sh
@@ -1,3 +1,3 @@
 APP_NAME=udacityazurecicd-23091201
-az webapp up --name $APP_NAME --resource-group Azuredevops --sku B1 --logs --runtime "PYTHON:3.9"
+az webapp up --name $APP_NAME --resource-group Azuredevops --sku B1 --runtime "PYTHON:3.9"
 echo "Deployed URL: https://$APP_NAME.azurewebsites.net"
