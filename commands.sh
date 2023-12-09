624d27c71d60ad68c99daa66db7026a5cc60f2e5
Thanh Le <letrithanh@outlook.com>
2023-12-09 14:16:33 +0700

    update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,3 @@
 APP_NAME=udacityazurecicd-230912
-az webapp up -n $APP_NAME
+az webapp up --name $APP_NAME --resource-group Azuredevops --sku B1 --logs --runtime "PYTHON:3.9"
 echo "Deployed URL: https://$APP_NAME.azurewebsites.net"
